c6fb01bb25df06da4960b49ba2b3440ae0deea67
MADHAV ANAND <m47903237@gmail.com>
2026-09-05 12:54:21 +0530

    Add complete JS logic

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let userScore = 0;
 let computerScore = 0;
-const choices = document.getElementsByClassName("choice");
+const choices = document.querySelectorAll(".choice");
 
-const draw
+const showWinner = (userWin) => {
+    if (userWin) {
+        console.log('You win');
+    }else{
+        console.log('You lose');
+    }
+}
+
+const drawGame = () => {
+    console.log('Draw')
+}
 
 const generateComputerChoice = () => {
     let options = ['rock' , 'paper' , 'scissors'];
@@ -12,17 +22,28 @@ const generateComputerChoice = () => {
 
 const playGame = (userChoice) => {
 
-const computerChoice = generateComputerChoice();
+let computerChoice = generateComputerChoice();
 console.log(computerChoice);
 if(computerChoice === userChoice) {
+drawGame();
+}else{
+    let userWin = true;
+    if(userChoice === 'rock' ){
+       userWin = computerChoice = 'paper' ? false : true;
 
+    }else if(userChoice === 'paper' ){
+        userWin = computerChoice = 'scissors' ? false : true;
+    }else{
+        userWin = computerChoice = 'rock' ? false: true;
+    }
+    showWinner(userWin);
 }
 }
 choices.forEach(choice => {
 
     choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute('id');
-        console.log('choice was clicked' , choiceID);
+        console.log('choice was clicked' );
         playGame(userChoice);
 
     })
