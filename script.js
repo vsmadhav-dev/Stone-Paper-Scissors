7e3556a1a618810c69c69d9d92e14c483570e799
MADHAV ANAND <m47903237@gmail.com>
2026-09-05 13:33:42 +0530

    Add final UI changes according to logic in game

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 let userScore = 0;
 let computerScore = 0;
+const userScorePara = document.querySelector("#user-score");
+const computerScorePara = document.querySelector("#computer-score");
 const choices = document.querySelectorAll(".choice");
+const msg= document.querySelector('.msg');
 
-const showWinner = (userWin) => {
+const showWinner = (userWin , userChoice, computerChoice) => {
     if (userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
         console.log('You win');
+        msg.innerText = `You win! Your ${userChoice} beats ${computerChoice}`;
+        msg.style.backgroundColor = 'green';
     }else{
+
+        computerScore++;
+        computerScorePara.innerText = computerScore;
+        // computerScorePara.innerHTML += computerScorePara.innerHTML;
         console.log('You lose');
+        msg.innerText = `You lose! ${computerChoice} beats Your ${userChoice}`;
+        msg.style.backgroundColor = 'red';
     }
 }
 
 const drawGame = () => {
     console.log('Draw')
+    msg.innerText = 'Game Draw! Play again';
+    msg.style.backgroundColor = '#081b31';
 }
 
 const generateComputerChoice = () => {
@@ -29,14 +44,14 @@ drawGame();
 }else{
     let userWin = true;
     if(userChoice === 'rock' ){
-       userWin = computerChoice = 'paper' ? false : true;
+       userWin = computerChoice === 'paper' ? false : true;
 
     }else if(userChoice === 'paper' ){
-        userWin = computerChoice = 'scissors' ? false : true;
+        userWin = computerChoice === 'scissors' ? false : true;
     }else{
-        userWin = computerChoice = 'rock' ? false: true;
+        userWin = computerChoice === 'rock' ? false: true;
     }
-    showWinner(userWin);
+    showWinner(userWin ,userChoice , computerChoice);
 }
 }
 choices.forEach(choice => {
